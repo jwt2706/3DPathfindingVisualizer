df46bc9312b4151c6c3be3110d66ac2106da9106
jwt2706 <jwt2home@gmail.com>
2024-03-16 09:33:10 -0400

    General: seo + build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/3DMazePathfinding/',
   build: {
     outDir: 'dist',
+    assetsInclude: ['**/*.txt'],
     rollupOptions: {
       input: {
         main: 'index.html',
